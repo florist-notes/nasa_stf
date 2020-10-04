cac1aa8e80d7d778dcb74ee3b9fcb666bc6e5ad4
SKKSaikia <ar5saikia@gmail.com>
2020-10-04 23:15:51 +0530

    final webapp progress

--- webapp_nasa/app/views.py
+++ webapp_nasa/app/views.py
@@ -1,5 +1,10 @@
 from app import app
 from flask import render_template, request, redirect, jsonify, make_response
+from datetime import datetime
+#import image_slicer
+import os
+#import numpy
+from werkzeug.utils import secure_filename
 
 
 @app.template_filter('clean_date')
@@ -16,6 +21,9 @@ def index():
 def map():
     return render_template('public/map2.html')
 
+@app.route('/upload')
+def upload():
+    return render_template('public/upload.html')
 
 @app.route('/fire')
 def fire():
@@ -28,6 +36,10 @@ def draw():
 @app.route('/inform')
 def inform():
     return render_template('public/inform.html')
+
+@app.route('/thankyou2')
+def thankyou2():
+    return render_template('public/thankyou2.html')
 
 @app.route('/thankyou')
 def thankyou():
@@ -72,3 +84,41 @@ def json():
 
 app.config["IMAGE_UPLOADS"] = "./app/static/img/uploads/"
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG", " GIF"]
+
+app.config["IMAGE_UPLOADS"] = "./app/static/img/uploads/" 
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG", " GIF"]
+
+
+def allowed_image(filename):
+    if not "." in filename:
+        return False
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+@app.route('/upload-image', methods=["GET","POST"])
+def upload_image():
+    if request.method == "POST":
+
+        if request.files:
+            image = request.files["image"]
+            if image.filename == "":
+                print("Image must have a filename")
+                return redirect(request.url)
+
+            if not allowed_image(image.filename):
+                print("That image extension is not allowed")
+                return redirect(request.url)
+            else:
+                filename = secure_filename(image.filename)
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+                #image_cut(filename)
+            print("Image saved")
+            filename = secure_filename(image.filename)
+            #return redirect(request.url)
+            return redirect('/thankyou2')
+
+    return render_template("public/upload_image.html")
